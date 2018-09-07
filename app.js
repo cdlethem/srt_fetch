7a77d7ae06f8f6ad0620d7de0f6f6555a05d8cfa
Colin Lethem <cdlethem@gmail.com>
2018-09-07 14:14:35 +0800

    Working API call and JSON formatting

--- app.js
+++ app.js
@@ -19,50 +19,69 @@ const podio = new Podio({
   clientId: clientId,
   clientSecret: clientSecret
 });
-results = {}
-results.rawData = [];
+
+processResponse = (responseData) => {
+  this.rawData = [];
+  this.cleanedData = {};
+  console.log("Matching filter = " + responseData.filtered +" , Returned = " + responseData.items.length);
+  this.rawData.push(...responseData.items);
+  console.log(this.rawData.length);
+
+  this.rawData.forEach((prospie) => {
+    Object.assign(this.cleanedData, { [prospie.item_id] : {"link":prospie.link} });
+    prospie.fields.forEach((field) => {
+      key = field.external_id;
+      if (field.type === "category" && !field.config.settings.multiple) {
+        value = field.values[0].value.text;
+      } else if (field.type === "category" && field.config.settings.multiple) {
+        n = field.values.length;
+        value = "";
+        for (i = 0; i < n; i++) {
+          value += field.values[i].value.text + ", "
+        };
+        } else if (field.type === "contact") {
+            value = field.values[0].value.name;
+          } else if (field.type === "app" && !field.config.settings.multiple) {
+              value = field.values[0].value.title;
+            } else if (field.type === "app" && field.config.settings.multiple) {
+                n = field.values.length;
+                value = "";
+                for (i = 0; i < n; i++) {
+                  value += field.values[i].value.title + ", "
+                };
+              } else if (field.type === "location") {
+                  value = field.values[0].formatted;
+                } else if (field.type === "date") {
+                    value = field.values[0].start_date;
+                  } else if ((!field.values.length) || (field.type === "image")) {
+                    value = "NA";
+                  } else value = field.values[0].value
+        Object.assign(this.cleanedData[prospie.item_id],{[key]:value});
+    });
+  });
+
+  return this.cleanedData
+}
 
 podio.authenticateWithApp(appId, appToken, (err) => {
-
+  this.results = {};
   if (err) throw new Error(err);
 
   podio.isAuthenticated().then(() => {
-
-    podio.request('POST', '/item/app/' + appId + '/filter/' + viewId, {"limit":500,"offset":500}).then((responseData) => {
-      console.log("Matching filter = " + responseData.filtered +" , Returned = " + responseData.items.length);
-
-      results.rawData.push(...responseData.items);
-      console.log(results.rawData.length);
-    });
-
-    podio.request('POST', '/item/app/' + appId + '/filter/' + viewId, {"limit":500,"offset":0}).then((responseData) => {
-      console.log("Matching filter = " + responseData.filtered +" , Returned = " + responseData.items.length);
-        
-      results.rawData.push(...responseData.items);
-      console.log(results.rawData.length);
-
-      results.cleanedData = {};
-
-      for(const prospie of results.rawData) {
-        Object.assign(results.cleanedData, {prospie.item_id: {
-          
-        }}
-          
-        })
-      }
-
-
-
-      fs.writeFile("srtfetch.json", JSON.stringify(results.rawData), (err) => {
+    podio.request('POST', '/item/app/' + appId + '/filter/' + viewId, {"limit":500,"offset":0,"sort_by":"created_on"})
+    .then((responseData) => {
+      Object.assign(this.results, processResponse(responseData));
+      return podio.request('POST', '/item/app/' + appId + '/filter/' + viewId, {"limit":500,"offset":500,"sort_by":"created_on"})
+  }).then((responseData) => {
+      Object.assign(this.results, processResponse(responseData));
+      return podio.request('POST', '/item/app/' + appId + '/filter/' + viewId, {"limit":500,"offset":1000,"sort_by":"created_on"})
+  }).then((responseData) => {
+      Object.assign(this.results, processResponse(responseData));
+      console.log(Object.keys(this.results).length);
+      fs.writeFile("srtfetch.json", JSON.stringify(this.results), (err) => {
         if (err) throw err;
         console.log("File Written");
       });
-    });
-
-    
-
-  }).catch(err => console.log(err));
-
+    });     
+  });
 });
-
-
--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     clientSecret: "yRVgOJ7Iedtkg2QQqCRFBQ8aYKWOqr7jNqzIVMg8NtXxiLJ7EZ1ryclo39FYMHTM",
     appId: 16938234,
     appToken: "3ef72bba267b4f71b84de25adf4716b2",
-    viewId: 38454017
+    viewId: 39305448
             };
 
 module.exports = config
